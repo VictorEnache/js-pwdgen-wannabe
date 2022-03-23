05acd5d994f029c057c81ee56f73b9801eff8945
VictorEnache <victor22enache@gmail.com>
2022-03-23 14:45:10 +0100

    add comment

--- main.js
+++ main.js
@@ -7,4 +7,5 @@ const last_name = prompt("scrivi qui sotto il tuo cognome");
 //chiedi all'utente il suo colore preferito
 const color = prompt("scrivi qui sotto il tuo colore preferito");
 
+//Scrivo sulla pagina il risultato + il numero 22
 document.getElementById("psw").innerHTML = `${first_name}${last_name}${color}` + "22";
